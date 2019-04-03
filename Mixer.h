9dca95b68477e306f957abb90a9d55330bfa4e85
Jeremy Arden <ardenjeremy@gmail.com>
2019-04-03 21:59:41 +0700

    Add files via upload

---
+++ Mixer.h
@@ -0,0 +1,14 @@
+#ifndef _MIXER_H
+#define _MIXER_H
+
+#include "Facility.h"
+#include <iostream>
+using namespace std;
+
+class Mixer : public Facility
+{
+    public:
+        Mixer();
+};
+
+#endif
