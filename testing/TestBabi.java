39605f73967bfe2e693f096c866551f834838b4c
rafizhafraan <44691344+rafizhafraan@users.noreply.github.com>
2019-04-21 04:05:43 +0700

    Update TestBabi.java

--- testing/TestBabi.java
+++ testing/TestBabi.java
@@ -12,6 +12,7 @@ import animal.Babi;
 public class TestBabi {
     private Babi babi;
 
+    @Test
     public void tesCtor() {
         babi = new Babi();
         assertEquals("Tes Name of Daging","dagingAyam",babi.getDagingBabi().getProdName());
@@ -21,11 +22,13 @@ public class TestBabi {
         assertEquals("Tes posisi Y",2,babi.getPosisiY());
     }
 
+    @Test
     public void tesIsBabiDead() {
         babi = new Babi();
         assertEquals("Tes isBabiDead",false,babi.isBabiDead());
     }
 
+    @Test
     public void tesMeat() {
         babi = new Babi();
         assertEquals("Tes Name of Daging","dagingAyam",babi.getDagingBabi().getProdName());
@@ -33,21 +36,25 @@ public class TestBabi {
         assertEquals("Tes amount of Daging",1,babi.getDagingBabi().getProdAmount());
     }
 
+    @Test
     public void tesIsMeatAnimal() {
         babi = new Babi();
         assertEquals("Tes isMeatAnimal",true,babi.isMeatAnimal());
     }
 
+    @Test
     public void tesGetPosisiX() {
         babi = new Babi();
-        assertEquals("Tes posisi X",0,babi.getPosisiX());
+        assertEquals("Tes posisi X",8,babi.getPosisiX());
     }
 
+    @Test
     public void tesGetPosisiY() {
         babi = new Babi();
-        assertEquals("Tes posisi Y",0,babi.getPosisiY());
+        assertEquals("Tes posisi Y",2,babi.getPosisiY());
     }
 
+    @Test
     public void tesRender() throws FileNotFoundException {
         babi = new Babi();
         Map m = new Map("/Users/abiyyuismunandar/Documents/OOP/TUBES/Engi's Farm/Peta/peta.txt");
